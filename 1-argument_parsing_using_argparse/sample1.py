aa718c20b8ed08410037a52b618863e40f0acd8b
Jorge André Alves <jorge.alves@dengun.com>
2017-07-10 15:06:12 +0100

    :tada: add module 1

---
+++ 1-argument_parsing_using_argparse/sample1.py
@@ -0,0 +1,16 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="A simple argument parser",
+    epilog="This is where you might put example usage"
+)
+
+parser.print_help()
+# usage:  __ed_file.py [-h]
+
+# A simple argument parser
+
+# optional arguments:
+#  -h, --help  show this help message and exit
+
+# This is where you might put example usage
